f76eb3dbab290d1ecd09f65e6ee84928ef46dd54
Ahmed Elgamal <ahmedelgamal5320@gmail.com>
2024-04-12 05:58:42 +0200

    01-intro

---
+++ 01-intro/src/App.java
@@ -0,0 +1,49 @@
+public class App {
+    public static void main(String[] args) {
+        /*
+         * Object-Oriented Programming (OOP): is a methodology or paradigm to design
+         * program using classes & objects
+         * ==========================
+         * OOP concepts:
+         * 1. Classes
+         * 2. Objects
+         * 3. Abstractions (interface)
+         * 4. Polymorphism
+         * 5. Encapsulation
+         * 6. Inheritance
+         * 7. Association
+         * 8. Composition
+         * 9. Aggregation
+         * ==========================
+         * Why OOP:
+         * 1. reusability
+         * 2. readability
+         * 3. high performance
+         * 4. space utilization
+         * ==========================
+         * Any thing is an object
+         * 
+         * Object consist of:
+         * 1. data, properties, or attributes
+         * 2. operations(), methods(), functions(), or behaviors()
+         * ==========================
+         * for example let make object-oriented analysis & design (OOAD) for car object:
+         * 1. properties:
+         * - no
+         * - color
+         * - brand
+         * - max speed
+         * 2. methods:
+         * - turn on
+         * - turn off
+         * - move
+         * - brake
+         * - set() & get() for any property for example: setColor() & getColor()
+         * ==========================
+         * Class:it is the template, prototype, ot blueprint for objects, and
+         * each object is instance of a class.
+         * ==========================
+         * Object or instance: is an executable copy of a class
+         */
+    }
+}
